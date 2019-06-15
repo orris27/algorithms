79902593c5baadabd11b28449977edf2e48519ab
orris27 <gsorris27@gmail.com>
2019-06-15 15:26:42 +0800

    update

--- leetcode/54-spiral-matrix/simulation.cpp
+++ leetcode/54-spiral-matrix/simulation.cpp
@@ -37,24 +37,17 @@ typedef long long LL;
 class Solution {
 public:
     int dirs[4][2] = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
-    //3, 2, 2, 1, 1
-    // 4, 2, 3, 1, 2
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> res;
         if (matrix.size() == 0)
             return res;
         int nums_x = matrix[0].size() - 1, nums_y = matrix.size() - 1;
-        for (int i =0;i<matrix[0].size();++i)
+        for (int i =0;i<matrix[0].size();++i) // move to the top right in order to ensure that our starting point would not participate in the operation in the loop
             res.push_back(matrix[0][i]);
-        int count =matrix[0].size(), size = matrix.size() * matrix[0].size();
+        int count = matrix[0].size(), size = matrix.size() * matrix[0].size();
         for(int x=0, y=matrix[0].size()-1, k=0;count<size;++k)
         {
-//            debug("k=%d\n", k);
-//            debug("(x, y) = (%d, %d)\n", x, y);
-//            debug("count=%d\n", count);
-//            if (count == 7)
-//                break;
-            if (k%2 == 0) // x
+            if (k%2 == 0) // move down to the bottom right or move up to the top left
             {
                 for (int i=0;i<nums_y;++i, ++count)
                 {
@@ -64,7 +57,7 @@ public:
                 }
                 --nums_y;
             }
-            else // y
+            else // move to the left or right
             {
                 for(int i=0;i<nums_x;++i, ++count)
                 {
